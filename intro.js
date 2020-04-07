60639a0965fd020cf68d3a23666359ed41f2da17
Silvia <silvia.alexa.silva@gmail.com>
2020-04-07 20:47:38 +0100

    Node.js & Chalk

---
+++ intro.js
@@ -0,0 +1,16 @@
+
+const chalk = require ('chalk')
+
+const wildPeople = ['Guilherme', 'Gonçalo', 'André', 'João']
+const randomChalk = () => {
+    chalkColors = ['pink', 'blue', 'yellow', 'orange']
+    const randomNumber = () => Math.round(Math.random() * chalkColors.length)
+    return chalkColors[randomNumber()]
+
+
+
+
+
+}
+
+wildPeople.map((wildPeople) => console.log(chalk[randomChalk()](wildPeople)))
