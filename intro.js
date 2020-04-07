15426aa1393d1d5fe9c2902d09a48ff9571607cc
Aivlis <55801093+silviaalexasilva@users.noreply.github.com>
2020-04-07 20:52:51 +0100

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,16 +1,10 @@
 
 const chalk = require ('chalk')
-
 const wildPeople = ['Guilherme', 'Gonçalo', 'André', 'João']
 const randomChalk = () => {
     chalkColors = ['pink', 'blue', 'yellow', 'orange']
     const randomNumber = () => Math.round(Math.random() * chalkColors.length)
     return chalkColors[randomNumber()]
-
-
-
-
-
 }
 
 wildPeople.map((wildPeople) => console.log(chalk[randomChalk()](wildPeople)))
